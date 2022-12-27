2961b7c4c766fcd0fa6504b132436eee30cdbcbb
unknown <adarshnaik.hb@gmail.com>
2022-12-27 18:06:05 +0530

    latest code

---
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.crm.qa.pages;
+
+public class LoginPage {
+	
+	public void test()
+	{
+		System.out.println("test");
+	}
+
+}
---
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -0,0 +1,11 @@
+package com.crm.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+
+  @Test
+  public void testTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
